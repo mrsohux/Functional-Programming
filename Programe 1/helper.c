02c6852c81572afe1785e8d7d82e01a2147afc52
MrSohux <55574445+mrsohux@users.noreply.github.com>
2020-07-22 13:45:30 +0530

    Create helper.c

---
+++ Programe 1/helper.c
@@ -0,0 +1,23 @@
+#include "header.h"
+
+
+////////////////////////////////////////////////////////
+// 
+//	Function Name	Addition
+//  Input			Integer,Integer
+//  Output			Integer
+// 	Description		It perform addition of two Numbers
+//	Date			22 July 2020
+//	Author			Sohel Riyaz Shaikh
+//
+////////////////////////////////////////////////
+
+
+int Addition(int iValue1,int iValue2)
+{
+	int iAns=0;
+	
+	iAns=iValue1+iValue2;
+	
+	return iAns;
+}
